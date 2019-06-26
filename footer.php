f09b5db377c9659267b3db0793c0d99b431829af
marfrancis <marcelo@mediasolutionproducoes.com.br>
2019-06-26 10:55:51 -0300

    revisão de links na home

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
-	<footer>    
-		<nav class="nav nav-fill rodape">
+<footer>    
+    <nav class="nav nav-fill rodape">
+   
+        <div class="col-md-2"> 
+        <li class="footer-logo-size">
+                <img src="img/logo_branco.png" width="200" alt="">
+        </li>  
+        </div>
 
         <div class="col-md-9">
               <ul class="nav marginFooter">                  
@@ -16,58 +22,43 @@
                       <a class="nav-link active menu-footer" href="contato.html">Contato</a>
                   </li>
 
-			<div class="col-md-9">
-				<ul class="nav marginFooter">                  
-					<li class="nav-item" >
-						<a class="nav-link active menu-footer" href="index.html">Home</a>
-						<a class="nav-link active menu-footer" href="sobre.html">Sobre</a>
-						<a class="nav-link active menu-footer" href="loja.html">Loja</a>
-					</li>
-					<li class="nav-item" >
-						<a class="nav-link active menu-footer" href="mapa.html">Mapa</a>
-						<a class="nav-link active menu-footer" href="FAQ.html">FAQ</a> 
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active menu-footer" href="contato.html">Contato</a>
-					</li>
+                  <div class="col-md-2 text-primary div-icones" >
+                    <ul class="ul-icones-footer ">
+                        <li class="nav-link active icones-footer">
+                            <a href="https://twitter.com/reeducarecicla" target="_blank">
+                                <i class="fa fa-twitter text-white"> </i>   
+                            </a>
+                        </li>
 
-					<div class="col-md-2 text-primary div-icones" >
-						<ul class="ul-icones-footer ">
-							<li class="nav-link active icones-footer">
-								<a href="https://twitter.com/reeducarecicla" target="_blank">
-									<i class="fa fa-twitter text-white"> </i>   
-								</a>
-							</li>
+                        <li class="nav-link active icones-footer">
+                            <a href="https://facebook.com/reeducarecicla" target="_blank">
+                                <i class="fa fa-facebook text-white"> </i>
+                            </a>
+                        </li> 
+                        <li class=" nav-link active icones-footer">
+                            <a href="http://instagram.com/reeducarecicla" target="_blank">
+                                <i class="fa fa-instagram text-white"> </i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                  <li class="nav-item newsletter">
+                     <label class="form-control newsletter-footer"> Assine nossa Newsletter!</label>                
+                        <div class="input-group newsletter-email">
+                             <input class="form-control" type="email" placeholder="email@email.com.br"> </input>
+                             <span class="input-group-btn "> 
+                             <button class="btn btn-success" class="btn-newsletter" type="submit">Assine agora</button>
+                          </span>
+                        </div>
+                  </li>
+              </ul>
+          </div>
+    </nav> 
 
-							<li class="nav-link active icones-footer">
-								<a href="https://facebook.com/reeducarecicla" target="_blank">
-									<i class="fa fa-facebook text-white"> </i>
-								</a>
-							</li> 
-							<li class=" nav-link active icones-footer">
-								<a href="http://instagram.com/reeducarecicla" target="_blank">
-									<i class="fa fa-instagram text-white"> </i>
-								</a>
-							</li>
-						</ul>
-					</div>
-					<li class="nav-item newsletter">
-						<label class="form-control newsletter-footer"> Assine nossa Newsletter!</label>                
-						<div class="input-group newsletter-email">
-							<input class="form-control" type="email" placeholder="email@email.com.br"> </input>
-							<span class="input-group-btn "> 
-								<button class="btn btn-success" class="btn-newsletter" type="submit">Assine agora</button>
-							</span>
-						</div>
-					</li>
-				</ul>
-			</div>
-		</nav> 
-
-		<section class="container-fluid" id="foot5">
-			<h4 class="text-center" id="footer-copyrights">Copyrights © 2019 ReeducaRecicla</h4>
-		</section>
-	</footer>
+      <section class="container-fluid" id="foot5">
+          <h4 class="text-center" id="footer-copyrights">Copyrights © 2019 ReeducaRecicla</h4>
+      </section>
+</footer>
 
 	<?php if (strpos($_SERVER['HTTP_USER_AGENT'], "Google Page Speed Insights") === false): ?>
 		<!-- O que tiver aqui dentro o page speed do Google não consegue ver -->
